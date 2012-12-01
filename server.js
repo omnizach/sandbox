8fcb738acfe89da92c00271854e2cbb15d481844
Zach Young <zach.young@gmail.com>
2012-12-01 14:00:47 -0800

    counter page changed

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     path = require('path'),
-    http = require('http');
+    http = require('http'),
+    fs = require('fs'),
+    util = require('util');;
     
 var app = express();
 
@@ -17,6 +19,21 @@ app.get('/', function(req, res) {
 
 app.get('/bottles', function(req, res) {
     res.sendfile('public/counter.html');
+});
+
+app.get('/salsa', function(req, res) {
+    // http://freemusicarchive.org/
+    var mp3File = __dirname + "/SONGO_21_-_05_-_De_Cuba.mp3";
+    var stat = fs.statSync(mp3File); // we need some info about the file size
+
+    res.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fs.createReadStream(mp3File);
+    // instead of events we'll make a single call to a helper function
+    util.pump(readStream, res);
 });
 
 http.createServer(app).listen(app.get('port'), function () {
